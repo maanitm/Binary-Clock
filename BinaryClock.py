411ad5174c7804cd9367316f5016c7f390c74e54
Maanit Madan <me@maanit.me>
2016-06-28 16:18:33 +0000

    Time converted to string, int and binary string.

--- BinaryClock.py
+++ BinaryClock.py
@@ -10,3 +10,18 @@ GPIO.setup(25,GPIO.OUT)
 GPIO.setup(5,GPIO.OUT)
 GPIO.setup(19,GPIO.OUT)
 GPIO.setup(21,GPIO.OUT)
+
+hourStr = time.strftime("%I")
+minuteStr = time.strftime("%M")
+
+hourInt = int(float(hourStr))
+minuteInt = int(float(minuteStr))
+
+hourBin = "{0:b}".format(hourInt)
+minuteBin = "{0:b}".format(minuteInt)
+
+print(hourInt)
+print(minuteInt)
+
+print(hourBin)
+print(minuteBin)
