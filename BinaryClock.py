7ee7201ac0f0f9050f5023b0d3fb6b1b0e18e184
Maanit Madan <me@maanit.me>
2016-06-28 03:33:15 +0000

    GPIO Setup complete

--- BinaryClock.py
+++ BinaryClock.py
@@ -1,2 +1,12 @@
 import RPi.GPIO as GPIO
 import time
+
+GPIO.setmode (GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(2,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(25,GPIO.OUT)
+GPIO.setup(5,GPIO.OUT)
+GPIO.setup(19,GPIO.OUT)
+GPIO.setup(21,GPIO.OUT)
